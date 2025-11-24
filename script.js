3561846ad5c270fdf1e833d7fe6c997aeadc2b22
hilahshklarski-cloud <hilah.shklarski@gmail.com>
2025-11-23 21:34:47 -0500

    script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ if (yearSpan) yearSpan.textContent = new Date().getFullYear();
 document.getElementById("helloBtn")?.addEventListener("click", () => {
   alert("Hello from your first site!");
 });
+
 function loadAuthors() {
     document.getElementById("tab_content").innerHTML = `
         <div class="authors-container">
